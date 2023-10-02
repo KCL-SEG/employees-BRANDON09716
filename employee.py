5c60c1db9a9dc48e8ed1b3ce3cab0658d131b543
BRANDON09716 <heng.2.zhang@kcl.ac.uk>
2023-10-02 15:16:20 +0100

    pass

--- employee.py
+++ employee.py
@@ -1,6 +1,3 @@
-"""Employee pay calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 class Employee:
     def __init__(self, name):
         self.name = name
@@ -12,20 +9,66 @@ class Employee:
         return self.name
 
 
-# Billie works on a monthly salary of 4000.  Their total pay is 4000.
-billie = Employee('Billie')
+class MonthlySalaryEmployee(Employee):
+    def __init__(self, name, monthly_salary, bonus=0):
+        super().__init__(name)
+        self.monthly_salary = monthly_salary
+        self.bonus = bonus
 
-# Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
-charlie = Employee('Charlie')
+    def get_pay(self):
+        return self.monthly_salary + self.bonus
 
-# Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
-renee = Employee('Renee')
+    def __str__(self):
+        if self.bonus > 0:
+            return f"{self.name} works on a monthly salary of {self.monthly_salary} and receives a bonus commission of {self.bonus}. Their total pay is {self.get_pay()}."
+        else:
+            return f"{self.name} works on a monthly salary of {self.monthly_salary}. Their total pay is {self.get_pay()}."
 
-# Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
-jan = Employee('Jan')
 
-# Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
-robbie = Employee('Robbie')
+class HourlyEmployee(Employee):
+    def __init__(self, name, hourly_rate, hours_worked, bonus=0):
+        super().__init__(name)
+        self.hourly_rate = hourly_rate
+        self.hours_worked = hours_worked
+        self.bonus = bonus
 
-# Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
-ariel = Employee('Ariel')
+    def get_pay(self):
+        return (self.hourly_rate * self.hours_worked) + self.bonus
+
+    def __str__(self):
+        if self.bonus > 0:
+            return f"{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour and receives a bonus commission of {self.bonus}. Their total pay is {self.get_pay()}."
+        else:
+            return f"{self.name} works on a contract of {self.hours_worked} hours at {self.hourly_rate}/hour. Their total pay is {self.get_pay()}."
+
+
+class CommissionEmployee(Employee):
+    def __init__(self, name, monthly_salary, hourly_rate, hours_worked, contracts_landed, commission_per_contract, bonus=0):
+        super().__init__(name)
+        self.monthly_salary = monthly_salary
+        self.hourly_rate = hourly_rate
+        self.hours_worked = hours_worked
+        self.contracts_landed = contracts_landed
+        self.commission_per_contract = commission_per_contract
+        self.bonus = bonus
+
+
+    def get_pay(self):
+        return (self.monthly_salary +self.hourly_rate * self.hours_worked+ self.contracts_landed * self.commission_per_contract) + self.bonus
+
+    def __str__(self):
+        if self.bonus > 0:
+            return f"{self.name} works on a monthly salary of {self.monthly_salary} and receives a bonus commission of {self.bonus}. Their total pay is {self.get_pay()}."
+        elif self.monthly_salary == 0:
+            return f"{self.name} works on a contract of {self.hourly_rate} hours at {self.hours_worked}/hour and receives a commission for {self.contracts_landed} contract(s) at {self.commission_per_contract}/contract. Their total pay is {self.get_pay()}."
+        else:
+            return f"{self.name} works on a monthly salary of {self.monthly_salary} and receives a commission for {self.contracts_landed} contract(s) at {self.commission_per_contract}/contract. Their total pay is {self.get_pay()}."
+
+
+# Create employees using the respective subclasses
+billie = MonthlySalaryEmployee('Billie', 4000)
+charlie = HourlyEmployee('Charlie', 25, 100)
+renee = CommissionEmployee('Renee', 3000, 0,0,4, 200)
+jan = CommissionEmployee('Jan', 0,150, 25, 3, 220)
+robbie = MonthlySalaryEmployee('Robbie', 2000, 1500)
+ariel = HourlyEmployee('Ariel', 30, 120, 600)
